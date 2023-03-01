e45254181d19c0d150106b47187ee6284180054a
Mattias Van Eetvelt <mattias.vaneetvelt@student.uclouvain.be>
2023-03-01 23:46:24 +0100

    sv::read() ok

--- hw1/codes/SparseVector.cpp
+++ hw1/codes/SparseVector.cpp
@@ -5,7 +5,10 @@
 
 SparseVector::SparseVector() : AbstractVector(0) // empty default constructor
 {
+    // à priori c'est OK
     nnz = 0; 
+    rowidx = NULL; 
+    nzval = NULL; 
 }
 
 SparseVector::SparseVector(int nnzero, int const *rowindex, double const *nzvalues, int size) : AbstractVector(size)
@@ -28,19 +31,31 @@ SparseVector::~SparseVector()
 double SparseVector::Read(int i) const
 {
     assert(i > -1);
-    assert(i < GetSize());
-    return *(nzval+i);  
+    assert(i < length(*this)); 
+
+    for (int j = 0; j < length(*this); j++)
+    {
+    if (*(rowidx+j) == i)
+        return *(nzval+j);
+    }
+    return 0.0;
 }
 
-
 SparseVector& SparseVector::operator=(const SparseVector& otherVector)
-{
-    rowidx = otherVector.rowidx ; 
-    nzval = otherVector.nzval ;
+{   
+    for(int i=0; i<length(otherVector); i++) // iterate over the size of otherVector
+    {
+        nzval[i] = otherVector.Read(i) ; // acces value of private member with member function
+        // rowidx[i] = otherVector.Read(i) ; // fonctionne pas car Read lis return seulement nzval[i]
+    }
     return *this ; 
 }
 
-int main()
+SparseVector SparseVector::operator+(const SparseVector& v1) const
 {
-    return 0;
+    SparseVector SumOfVector; 
+    SumOfVector = *this ; 
+
+    return v1; 
 }
+
