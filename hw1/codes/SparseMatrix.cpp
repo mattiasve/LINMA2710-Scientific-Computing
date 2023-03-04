9b6ad8384901a359a4073a620e45162b1b62fa8e
Mattias Van Eetvelt <mattias.vaneetvelt@student.uclouvain.be>
2023-03-04 19:58:22 +0100

    operators implemented; constructor and * w/ vector to do

--- hw1/codes/SparseMatrix.cpp
+++ hw1/codes/SparseMatrix.cpp
@@ -9,7 +9,7 @@ SparseMatrix::SparseMatrix(int numRows, int numCols)
     for (int i=0; i<numRows; i++)
     {
         for (int j=0; j<numCols; j++)
-            {sparse_matrix[i][j]=0; }
+            sparse_matrix[i][j]=0;
     }
 }
 
@@ -40,10 +40,10 @@ SparseMatrix::SparseMatrix(int nnz, int const *ridx, int const *cidx, double con
     return 0.0;
 }
 
-// accesor 
+// accessor 
 int SparseMatrix::GetSize(int i) const
 {
-    assert(i==0 || i==1); // only two values of i are accepted 
+    assert(i==1 || i==2); // only two values of i are accepted 
     if (i == 1)
         return this->m;
     else
@@ -56,4 +56,62 @@ SparseMatrix::~SparseMatrix()
     delete[] colptr; 
     delete[] rowidx; 
     delete[] nzval; 
+}
+
+// overload assignment operator
+SparseMatrix& SparseMatrix::operator=(const SparseMatrix& otherSparseMatrix)
+{
+    this->m = otherSparseMatrix.m;
+    this->n = otherSparseMatrix.n; 
+    this->nnz = otherSparseMatrix.nnz; 
+    this->colptr = otherSparseMatrix.colptr;
+    this->rowidx = otherSparseMatrix.rowidx;
+    this->nzval = otherSparseMatrix.nzval;
+
+    return *this;
+}
+
+// unary +
+SparseMatrix SparseMatrix::operator+() const
+{
+    double *new_nzvalues;
+    new_nzvalues = new double[this->nnz];
+
+    for (int i=0; i<this->nnz; i++)
+    {
+        new_nzvalues[i] = + this->nzval[i]; 
+    }
+
+    SparseMatrix new_plus_sm(this->nnz, this->rowidx, this->colptr, new_nzvalues, this->GetSize(1), this->GetSize(2));
+    return new_plus_sm;
+}
+
+// unary -
+SparseMatrix SparseMatrix::operator-() const
+{
+    double *new_nzvalues;
+    new_nzvalues = new double[this->nnz];
+
+    for (int i=0; i<this->nnz; i++)
+    {
+        new_nzvalues[i] = - this->nzval[i]; 
+    }
+
+    SparseMatrix new_minus_sm(this->nnz, this->rowidx, this->colptr, new_nzvalues, this->GetSize(1), this->GetSize(2));
+    return new_minus_sm;
+}
+
+// scalar multiplication
+SparseMatrix SparseMatrix::operator *(double a) const
+{
+    double *new_nzvalues;
+    new_nzvalues = new double[this->nnz];
+
+    for (int i=0; i<this->nnz; i++)
+    {
+        new_nzvalues[i] = a * this->nzval[i]; 
+    }
+
+    SparseMatrix new_multiply_sm(this->nnz, this->rowidx, this->colptr, new_nzvalues, this->GetSize(1), this->GetSize(2));
+    return new_multiply_sm;
 }
